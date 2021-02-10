d782c1c2eed755dad5b73c010dcb912aa5c2d6f3
KzrN <pup3721yu@gmail.com>
2021-02-10 18:24:44 +0900

    header-new-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  get "users/account"
 end
